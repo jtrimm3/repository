070e5e90b42cd5c7a1b7cdc1a04ff7108ca48795
Peter Koplik <pkoplik24@gmail.com>
2015-08-31 14:30:49 -0400

    Sup guys

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,9 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+		String twoCharacters = input.substring(0,2);
+		String rest = input.substring(2, input.length());
+		return rest + twoCharacters;
 	}
 	
 	/**
