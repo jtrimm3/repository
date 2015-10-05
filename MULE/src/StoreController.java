cb7d0ac8952df452057c104d2c5d44f2d2639422
bsmit170 <benjamin.smith@gatech.edu>
2015-10-05 18:17:21 -0400

    Store controller got messed up by me :(
    fixed it

--- MULE/src/StoreController.java
+++ MULE/src/StoreController.java
@@ -76,42 +76,11 @@ public class StoreController implements Initializable, Controller{
 
     @FXML
     private void confirmBuy(ActionEvent event) throws IOException {
-<<<<<<< HEAD
-        amountEntered = Integer.valueOf(buyAmount.getText());
-        if (!muleModel.getLevel().equals("Beginner")) {
-            muleModel.initializeBuyDataOther(boughtItem, amountEntered, buyItems);
-            if (boughtItem.equals("Food")) {
-                foodOther = Math.max(foodOther - amountEntered, 0);
-            } else if (boughtItem.equals("Energy")) {
-                energyOther = Math.max(energyOther - amountEntered, 0);
-            } else if (boughtItem.equals("Smithore")) {
-                smithoreOther = Math.max(smithoreOther - amountEntered, 0);
-            } else if (boughtItem.equals("Crystite")) {
-                crystiteOther = Math.max(crystiteOther - amountEntered, 0);
-            } else {
-                muleOther = Math.max(muleOther - amountEntered, 0);
-                muleModel.enterMuleConfig();
-            }
-        } else {
-            muleModel.initializeBuyDataBeginner(boughtItem, amountEntered, buyItems);
-            if (boughtItem.equals("Food")) {
-                foodBeg = Math.max(foodBeg - amountEntered, 0);
-            } else if (boughtItem.equals("Energy")) {
-                energyBeg = Math.max(energyBeg - amountEntered, 0);
-            } else if (boughtItem.equals("Smithore")) {
-                smithoreBeg = Math.max(smithoreBeg - amountEntered, 0);
-            } else if (boughtItem.equals("Crystite")) {
-                crystiteBeg = Math.max(crystiteBeg - amountEntered, 0);
-            } else {
-                muleBeg = Math.max(muleBeg - amountEntered, 0);
-                muleModel.enterMuleConfig();
-            }
-        }
-        muleModel.buyResource();
-=======
+
+
         int amountBought = Integer.valueOf(buyAmount.getText());
         muleModel.buyResource(boughtItem,amountBought);
->>>>>>> d5f045014bb1cc38e0c6004bbb5b894586ab4898
+
     }
 
     @FXML
